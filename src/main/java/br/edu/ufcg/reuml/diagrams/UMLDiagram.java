dcacbb75aac443f4e68548932847fe4cb8925fe1
Gustavo Alves <gugamalves@yahoo.com.br>
2015-08-11 00:13:22 -0300

    Add profiles and diagrams

---
+++ src/main/java/br/edu/ufcg/reuml/diagrams/UMLDiagram.java
@@ -0,0 +1,54 @@
+package br.edu.ufcg.reuml.diagrams;
+
+import br.edu.ufcg.reuml.profiles.UMLProfile;
+import javax.persistence.*;
+
+/**
+ * An generic UML Diagram.
+ * Created by gustavo on 26/03/15.
+ */
+@Entity
+public abstract class UMLDiagram {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @OneToOne(cascade=CascadeType.ALL)
+    @JoinColumn
+    private UMLProfile profile;
+
+    @Column
+    private String name;
+
+    public UMLDiagram() {
+    }
+
+    public UMLDiagram(String name, UMLProfile profile) {
+        this.name = name;
+        this.profile = profile;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public UMLProfile getProfile() {
+        return profile;
+    }
+
+    public void setProfile(UMLProfile profile) {
+        this.profile = profile;
+    }
+}
